dba3d9cce41e0b8c98219855356d1bd7ca2aff9b
AndrewPang <thpangab@connect.ust.hk>
2018-08-10 22:35:45 +0800

    Include add new todo function and fab button fixed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -36,27 +36,27 @@
         <v-icon>edit</v-icon>
       </v-btn> -->
 
-      <v-btn fab dark small color="indigo">
-        <!-- <v-icon>add</v-icon> -->
-        <v-layout row justify-center>
+      <v-btn fab dark small color="indigo" @click.stop="dialog = true">
+        <v-icon>add</v-icon>
+      </v-btn>
+
+      <v-layout row justify-center>
         <v-dialog v-model="dialog" persistent max-width="400">
-          <v-icon slot="activator">add</v-icon>
           <v-card>
             <v-card-title class="headline">Add Todo Item</v-card-title>
             <v-container grid-list-md>
               <v-layout wrap>
-                <v-flex xs12><v-text-field label="Title"></v-text-field></v-flex>
+                <v-flex xs12><v-text-field v-model="newTodoTitle" label="Title"></v-text-field></v-flex>
               </v-layout>
             </v-container>
             <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn color="red darken-1" flat @click.native="dialog = false">Cancel</v-btn>
-              <v-btn color="green darken-1" flat @click.native="dialog = false">Add</v-btn>
+              <v-btn color="green darken-1" flat @click.native="dialog = false" @click="addTodo">Add</v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
-        </v-layout>
-      </v-btn>
+      </v-layout>
 
       <v-btn fab dark small color="red">
         <v-icon>delete</v-icon>
@@ -95,13 +95,20 @@ export default {
       bottom: true,
       left: false,
       transition: 'slide-y-reverse-transition',
-      // *** Dialog ***
-      dialog: false
+      // *** Dialog for New Todo ***
+      dialog: false,
+      newTodoTitle: ''
     };
   },
   methods: {
     checkItem(item) {
       item.checked = !item.checked;
+    },
+    addTodo() {
+      this.todos.push({
+        title: this.newTodoTitle,
+        checked: false
+      });
     }
   }
 }
