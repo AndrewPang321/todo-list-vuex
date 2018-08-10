75dc0fcb810e397405096545b46cd360847bc6a4
AndrewPang <thpangab@connect.ust.hk>
2018-08-10 23:22:15 +0800

    Include delete function and dialog

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,8 +3,9 @@
     <v-container fluid>
       <v-slide-y-transition mode="out-in">
         <v-layout column align-center>
+          <h2 v-if="todos.length == 0">You don't have any todo items!</h2>
           <v-list class="todoContainer">
-            <v-list-tile v-for="(item, i) in todos" :key="i" @click.stop="checkItem(item)">
+            <v-list-tile v-for="(item, i) in todos" :key="i" @click.stop="checkTodo(item)">
                 <v-list-tile-action>
                   <v-checkbox v-model="item.checked" color="primary"></v-checkbox>
                 </v-list-tile-action>
@@ -36,31 +37,58 @@
         <v-icon>edit</v-icon>
       </v-btn> -->
 
-      <v-btn fab dark small color="indigo" @click.stop="dialog = true">
+      <v-btn fab dark small color="indigo" @click.stop="addDialog = true">
         <v-icon>add</v-icon>
       </v-btn>
 
       <v-layout row justify-center>
-        <v-dialog v-model="dialog" persistent max-width="400">
+        <v-dialog v-model="addDialog" persistent max-width="400">
           <v-card>
             <v-card-title class="headline">Add Todo Item</v-card-title>
             <v-container grid-list-md>
               <v-layout wrap>
-                <v-flex xs12><v-text-field v-model="newTodoTitle" label="Title"></v-text-field></v-flex>
+                <v-flex xs12><v-text-field v-model="newTodoTitle" label="Title" autofocus></v-text-field></v-flex>
               </v-layout>
             </v-container>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="red darken-1" flat @click.native="dialog = false">Cancel</v-btn>
-              <v-btn color="green darken-1" flat @click.native="dialog = false" @click="addTodo">Add</v-btn>
+              <v-btn color="red darken-1" flat @click.native="addDialog = false">Cancel</v-btn>
+              <v-btn color="green darken-1" flat @click.native="addDialog = false" @click="addTodo">Add</v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
       </v-layout>
 
-      <v-btn fab dark small color="red">
+      <v-btn fab dark small color="red" @click.stop="deleteDialog = true">
         <v-icon>delete</v-icon>
       </v-btn>
+
+      <v-layout row justify-center>
+        <v-dialog v-model="deleteDialog" persistent max-width="400">
+          <v-card>
+            <v-card-title class="headline">Delete Todo Items</v-card-title>
+            <v-container grid-list-md>
+              <v-layout wrap>
+                <v-list class="deleteTodoContainer">
+                  <v-list-tile v-for="(item, i) in todos" :key="i" @click.stop="checkDeleteTodo(item)">
+                      <v-list-tile-action>
+                        <v-checkbox v-model="item.delete" color="primary"></v-checkbox>
+                      </v-list-tile-action>
+                      <v-list-tile-content>
+                        {{item.title}}
+                      </v-list-tile-content>
+                  </v-list-tile>
+                </v-list>
+              </v-layout>
+            </v-container>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="red darken-1" flat @click.native="deleteDialog = false">Cancel</v-btn>
+              <v-btn color="green darken-1" flat @click.native="deleteDialog = false" @click="deleteTodo">Delete</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-layout>
       
     </v-speed-dial>
   </v-card>
@@ -73,15 +101,18 @@ export default {
       todos: [
         {
           title: 'todo1',
-          checked: true
+          checked: true,
+          delete: false
         },
         {
           title: 'todo2',
-          checked: false
+          checked: false,
+          delete: false
         },
         {
           title: 'todo3',
-          checked: true
+          checked: true,
+          delete: false
         }
       ],
       // *** Float Button ***
@@ -95,19 +126,30 @@ export default {
       bottom: true,
       left: false,
       transition: 'slide-y-reverse-transition',
-      // *** Dialog for New Todo ***
-      dialog: false,
-      newTodoTitle: ''
+      // *** Dialog for Adding New Todo ***
+      addDialog: false,
+      newTodoTitle: '',
+      // *** Dialog for Deleting Todos ***
+      deleteDialog: false,
     };
   },
   methods: {
-    checkItem(item) {
+    checkTodo(item) {
       item.checked = !item.checked;
     },
     addTodo() {
       this.todos.push({
         title: this.newTodoTitle,
-        checked: false
+        checked: false,
+        delete: false
+      });
+    },
+    checkDeleteTodo(item) {
+      item.delete = !item.delete;
+    },
+    deleteTodo() {
+      this.todos = this.todos.filter(item => {
+        return item.delete == false
       });
     }
   }
@@ -133,6 +175,9 @@ a {
 }
 .todoContainer {
   min-width: 75%;
+}
+.deleteTodoContainer {
+  min-width: 100%
 }
 #create {
   min-height: 100%;
